0e27c9a0cdf8771ca9681f164afbfabbc2c1daa4
kenneth <jinzarchive@gmail.com>
2025-08-26 09:47:53 -0600

    Java interfeces added
    this interfaces are usefull for get generics methods.
    
    for frms and Cruds

---
+++ src/Interfaces/FrmInterface.java
@@ -0,0 +1,20 @@
+package Interfaces;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+/**
+ *
+ * @author unknown
+ */
+public interface FrmInterface {
+    public void clear();
+    public boolean validateRequiere();
+    public void save();
+    public void update();
+    public void delete();
+    public void search();
+    public void showData();
+}
